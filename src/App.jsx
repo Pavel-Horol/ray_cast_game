ed3ce082b8077dcbea82dc72124263494b986928
Pasha Horol <pashahorolskij4@gmail.com>
2024-05-29 13:22:24 +0300

    setup pixi js canvas into react

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import './App.css';
 import { useMemo } from 'react';
-
 import { BlurFilter, TextStyle } from 'pixi.js';
-import { Stage, Container, Sprite, Text } from '@pixi/react';
+import {Stage, Container, Sprite, Text, render, PixiComponent} from '@pixi/react';
+import Pixi from "./Pixi.jsx";
 
 export const App = () => {
-    const blurFilter = useMemo(() => new BlurFilter(2), []);
-    const bunnyUrl = 'https://pixijs.io/pixi-react/img/bunny.png';
     return (
-        <Stage x={800} y={600} options={{ background: 0x1099bb }}>
-            <Sprite image={bunnyUrl} x={300} y={150} />
-            <Sprite image={bunnyUrl} x={500} y={150} />
-            <Sprite image={bunnyUrl} x={400} y={200} />
-
-            <Container x={200} y={200}>
-            </Container>
-        </Stage>
+        <div>
+            <h1>My Portfolio</h1>
+            <Pixi/>
+        </div>
     );
 };
+
