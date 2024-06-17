5398d52a567452ddb7227d492882d3bfba39c90f
Pasha Horol <pashahorolskij4@gmail.com>
2024-06-17 15:33:02 +0300

    wall textures added

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,6 +1,7 @@
 import GameMap from "./Map";
 import {map} from "./assets";
 import {Player} from "./Player";
+import {i} from "vite/dist/node/types.d-aGj9QkWt";
 
 export default class GameWindow {
     private readonly FPS: number = 60;
@@ -16,9 +17,10 @@ export default class GameWindow {
     private readonly HALF_FOV = this.FOV / 2;
     private readonly STEP_ANGLE = this.FOV / this.WIDTH;
     private readonly DOUBLE_PI: number = 2 * Math.PI;
+    private readonly bgImage: HTMLImageElement
 
     canvas: HTMLCanvasElement;
-    context: CanvasRenderingContext2D;
+    context!: CanvasRenderingContext2D;
 
     cycleDelay: number = 1000 / this.FPS;
     oldCycleTime: number = Date.now();
@@ -28,14 +30,27 @@ export default class GameWindow {
 
     map: GameMap
     player: Player
+
+
+    private readonly walls: HTMLImageElement[] = []
     constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
+
         this.canvas = canvas;
         this.context = context;
         this.canvas.height = this.HEIGHT;
         this.canvas.width = this.WIDTH;
 
+        this.bgImage = document.createElement('img')
+        this.bgImage.src = 'src/assets/bg.png'
+
         this.map = new GameMap(this.context)
         this.player = new Player(this.context, this.map.MAP_SPEED, this.map.MAP_SCALE, this.map.MAP_SIZE)
+
+        for(let filename = 0; filename < 3; filename++){
+            let image = document.createElement('img')
+            image.src = 'src/assets/walls/' + filename + '.png'
+            this.walls.push(image)
+        }
 
         this.gameLoop(0);
     }
@@ -51,7 +66,6 @@ export default class GameWindow {
 
             this.player.updatePosition()
 
-            this.showFPS();
 
             //ray casting
             let currentAngle = this.player.angle + this.HALF_FOV
@@ -59,7 +73,7 @@ export default class GameWindow {
             let rayStartX = Math.floor(this.player.x / this.map.MAP_SCALE) * this.map.MAP_SCALE
             let rayStartY = Math.floor(this.player.y / this.map.MAP_SCALE) * this.map.MAP_SCALE
 
-           //loop for casted rays
+           //loop for cast rays
             for(let ray = 0; ray < this.WIDTH; ray ++){
 
                 let currentSin = Math.sin(currentAngle); currentSin = currentSin ? currentSin : 0.000001
@@ -67,6 +81,8 @@ export default class GameWindow {
 
                 // vertical intersection
                 let rayEndX, rayEndY, rayDirectionX, verticalDepth = 0;
+                let textureEndY: number
+                let textureY: number
                 if (currentSin > 0) {
                     rayEndX = rayStartX + this.map.MAP_SCALE
                     rayDirectionX = 1
@@ -84,12 +100,15 @@ export default class GameWindow {
 
                     let targetSquare = mapTargetY * this.map.MAP_SIZE + mapTargetX
                     if(targetSquare < 0 || targetSquare > this.map.MAP.length - 1) break
-                    if(map[targetSquare] !== 0) break
+                    if(map[targetSquare] !== 0) {textureY = map[targetSquare]; break }
                     rayEndX += rayDirectionX * this.map.MAP_SCALE
-                }
 
+                }
+                textureEndY = rayEndY
                 // vertical intersection
                 let  rayDirectionY, horizontalDepth = 0;
+                let textureEndX: number
+                let textureX: number
                 if (currentCos > 0) { rayEndY = rayStartY + this.map.MAP_SCALE; rayDirectionY = 1 }
                 else { rayEndY = rayStartY; rayDirectionY = -1 }
 
@@ -102,25 +121,44 @@ export default class GameWindow {
 
                     let targetSquare = mapTargetY * this.map.MAP_SIZE + mapTargetX
                     if(targetSquare < 0 || targetSquare > this.map.MAP.length - 1) break
-                    if(map[targetSquare] !== 0) break
+                    if(map[targetSquare] !== 0) { textureX = map[targetSquare]; break }
                     rayEndY += rayDirectionY * this.map.MAP_SCALE
                 }
+                textureEndX = rayEndX
                 //render 3d
+                let textureOffset = verticalDepth < horizontalDepth ? textureEndY : textureEndX
+                textureOffset = Math.floor( textureOffset - Math.floor(textureOffset / this.map.MAP_SCALE) * this.map.MAP_SCALE)
+
                 let depth = verticalDepth < horizontalDepth ? verticalDepth : horizontalDepth
-                let wallHeight = Math.min(this.map.MAP_SCALE * 300 / (depth + 0.0001), this.HEIGHT)
-                this.context.fillStyle = '#aaa'
-                this.context.fillRect(this.map.mapOffsetX + ray, this.map.mapOffsetY + (this.HEIGHT / 2 - wallHeight / 2), 1, wallHeight )
+                let wallHeight = Math.min(Math.floor(this.map.MAP_SCALE * 256 / (depth + 0.0001)), 5000)
+
+                let textureImage =  verticalDepth < horizontalDepth ? textureY! : textureX!
+
+                // this.context.fillStyle = verticalDepth < horizontalDepth ? '#aaa' : '#333'
+                // this.context.fillRect(this.map.mapOffsetX + ray, this.map.mapOffsetY + (this.HEIGHT / 2 - wallHeight / 2), 1, wallHeight )
+
+                // draw image
+                    this.context.drawImage(
+                        this.walls[textureImage],
+                        textureOffset,
+                        0,
+                        1,
+                        128,
+                        this.map.mapOffsetX + ray,
+                        this.map.mapOffsetY + (this.HALF_HEIGHT - Math.floor(wallHeight / 2)),
+                        1,
+                        wallHeight,
+                    )
 
                 currentAngle -= this.STEP_ANGLE
             }
-
-
-
 
             if(this.map.isShow){
                 this.map.draw(this.WIDTH, this.HEIGHT)
                 this.player.draw(this.map.mapOffsetX, this.map.mapOffsetY)
             }
+            this.showFPS();
+
         }
         requestAnimationFrame(this.gameLoop);
     }
@@ -131,14 +169,13 @@ export default class GameWindow {
     private updateScreen() {
         this.context.fillStyle = 'Black';
         this.context.fillRect(0, 0, this.WIDTH, this.HEIGHT);
+        this.context.drawImage(this.bgImage, 0, 0)
     }
-
     private showFPS() {
         this.context.fillStyle = 'White';
         this.context.font = '12px Monospace';
         this.context.fillText('FPS: ' + this.fps_rate, 10, 20);
     }
-
     private calcFPS() {
         this.cycleCount++;
         if (this.cycleCount >= 60) this.cycleCount = 0;
@@ -147,7 +184,6 @@ export default class GameWindow {
         this.oldCycleTime = startTime;
         if (this.cycleCount % 60 === 0) this.fps_rate = `${Math.floor(1000 / cycleTime)}`;
     }
-
     private showDebug() {
         this.context.fillStyle = 'White';
         this.context.font = '12px Monospace';
--- src/game/Player.ts
+++ src/game/Player.ts
@@ -1,21 +1,21 @@
 import {map} from "./assets";
 
 export class Player {
-    x: number
-    y: number
-    angle: number = Math.PI / 3
-    moveX: number = 0
-    moveY: number = 0
-    moveAngle: number = 0
-    radius: number = 5
+    x: number;
+    y: number;
+    angle: number = Math.PI / 3;
+    moveX: number = 0;
+    moveY: number = 0;
+    moveAngle: number = 0;
+    radius: number = 5;
 
-    //for debug
-    pMapX: number = 0
-    pMapY: number = 0
-    pOffsetX = 0
-    pOffsetY = 0
-    mTargetX = 0
-    mTargetY = 0
+    // for debug
+    pMapX: number = 0;
+    pMapY: number = 0;
+    pOffsetX = 0;
+    pOffsetY = 0;
+    mTargetX = 0;
+    mTargetY = 0;
 
     constructor(
         private readonly context: CanvasRenderingContext2D,
@@ -23,52 +23,93 @@ export class Player {
         private readonly mapScale: number,
         private readonly mapSize: number,
     ) {
-        this.x = this.mapScale + 20
-        this.y = this.mapScale + 20
+        this.x = this.mapScale + 20;
+        this.y = this.mapScale + 20;
     }
 
-    moveForward() { this.moveX = 1; this.moveY = 1; }
-    moveBackward(){ this.moveX = -1; this.moveY = -1; }
-    lookLeft() { this.moveAngle = 1 }
-    lookRight() { this.moveAngle = -1 }
-    resetMoveXY() { this.moveX = 0; this.moveY = 0 }
-    resetMoveAngle(){ this.moveAngle = 0 }
+    moveForward() { this.moveY = 1; }
+    moveBackward() { this.moveY = -1; }
+    moveRight() { this.moveX = -1; }
+    moveLeft() { this.moveX = 1; }
+    lookLeft() { this.moveAngle = 1; }
+    lookRight() { this.moveAngle = -1; }
+    resetMoveAngle() { this.moveAngle = 0; }
+    resetMoveX() { this.moveX = 0; }
+    resetMoveY() { this.moveY = 0; }
 
-    draw(mapOffsetX:number, mapOffsetY: number){
+    draw(mapOffsetX: number, mapOffsetY: number) {
         // player
-        let playerMapX = (this.x / this.mapScale) * 5  + mapOffsetX
-        let playerMapY = (this.y / this.mapScale) * 5 + mapOffsetY
-        this.pMapX = playerMapX
-        this.pMapY = playerMapY
+        let playerMapX = (this.x / this.mapScale) * 5 + mapOffsetX + 700;
+        let playerMapY = (this.y / this.mapScale) * 5 + mapOffsetY;
+        this.pMapX = playerMapX;
+        this.pMapY = playerMapY;
 
-        this.context.fillStyle = "Red"
+        this.context.fillStyle = "Red";
         this.context.beginPath();
         this.context.arc(
             playerMapX,
             playerMapY,
             this.radius,
             0,
-            Math.PI * 2)
-        this.context.fill()
-        this.context.closePath()
-
+            Math.PI * 2
+        );
+        this.context.fill();
+        this.context.closePath();
     }
 
-    updatePosition(){
-        let playerOffsetX = Math.sin(this.angle) * this.mapSpeed
-        let playerOffsetY = Math.cos(this.angle) * this.mapSpeed
+    updatePosition() {
+        let forwardOffsetX = Math.sin(this.angle) * this.mapSpeed * this.moveY;
+        let forwardOffsetY = Math.cos(this.angle) * this.mapSpeed * this.moveY;
+        let strafeOffsetX = Math.cos(this.angle) * this.mapSpeed * this.moveX * 0.5;
+        let strafeOffsetY = -Math.sin(this.angle) * this.mapSpeed * this.moveX * 0.5;
 
-        let mapTargetX = Math.floor(this.y / this.mapScale) * this.mapSize + Math.floor((this.x + playerOffsetX * this.moveX) / this.mapScale)
-        let mapTargetY = Math.floor(this.x / this.mapScale) * this.mapSize + Math.floor((this.y + playerOffsetY * this.moveY) / this.mapScale)
+        let newX = this.x + forwardOffsetX + strafeOffsetX ;
+        let newY = this.y + forwardOffsetY + strafeOffsetY ;
 
-        this.pOffsetX = playerOffsetX
-        this.pOffsetY = playerOffsetY
-        this.mTargetX = mapTargetX
-        this.mTargetY = mapTargetY
+        let mapTargetX = Math.floor(newY / this.mapScale) * this.mapSize + Math.floor(newX / this.mapScale);
+        let mapTargetY = Math.floor(newX / this.mapScale) * this.mapSize + Math.floor(newY / this.mapScale);
 
-        if(this.moveX && map[mapTargetX] === 0) this.x += playerOffsetX * this.moveX
-        if(this.moveY && map[mapTargetY] === 0) this.y += playerOffsetY * this.moveY
-        if(this.moveAngle) this.angle += 0.03 * this.moveAngle
+        this.pOffsetX = forwardOffsetX + strafeOffsetX;
+        this.pOffsetY = forwardOffsetY + strafeOffsetY;
+        this.mTargetX = mapTargetX;
+        this.mTargetY = mapTargetY;
 
+        let newPosX = this.x + forwardOffsetX;
+        let newPosY = this.y + forwardOffsetY;
+        let mapPosX = Math.floor(newPosX / this.mapScale);
+        let mapPosY = Math.floor(newPosY / this.mapScale);
+
+        if (map[mapPosY * this.mapSize + mapPosX] === 0) {
+            this.x = newPosX;
+            this.y = newPosY;
+        } else {
+            if (map[Math.floor((this.y + forwardOffsetY) / this.mapScale) * this.mapSize + Math.floor(this.x / this.mapScale)] === 0) {
+                this.y += forwardOffsetY;
+            }
+            if (map[Math.floor(this.y / this.mapScale) * this.mapSize + Math.floor((this.x + forwardOffsetX) / this.mapScale)] === 0) {
+                this.x += forwardOffsetX;
+            }
+        }
+
+        newPosX = this.x + strafeOffsetX;
+        newPosY = this.y + strafeOffsetY;
+        mapPosX = Math.floor(newPosX / this.mapScale);
+        mapPosY = Math.floor(newPosY / this.mapScale);
+
+        if (map[mapPosY * this.mapSize + mapPosX] === 0) {
+            this.x = newPosX;
+            this.y = newPosY;
+        } else {
+            if (map[Math.floor((this.y + strafeOffsetY) / this.mapScale) * this.mapSize + Math.floor(this.x / this.mapScale)] === 0) {
+                this.y += strafeOffsetY;
+            }
+            if (map[Math.floor(this.y / this.mapScale) * this.mapSize + Math.floor((this.x + strafeOffsetX) / this.mapScale)] === 0) {
+                this.x += strafeOffsetX;
+            }
+        }
+
+        if (this.moveAngle) {
+            this.angle += 0.03 * this.moveAngle;
+        }
     }
 }
